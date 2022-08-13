2d3e1e03f369d1131c20f82fcb06f4391a71f998
maypok86 <alex.mayshev.86@gmail.com>
2022-08-14 02:15:06 +0700

    [Chore] Add tests for argon2 hasher new

--- pkg/hash/argon2_test.go
+++ pkg/hash/argon2_test.go
@@ -1,12 +1,28 @@
 package hash_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/bxcodec/faker/v3"
 	"github.com/maypok86/conduit/pkg/hash"
 	"github.com/stretchr/testify/require"
 )
+
+func TestNewArgon2Hasher(t *testing.T) {
+	t.Parallel()
+
+	hasher := hash.NewArgon2Hasher(
+		hash.Time(2),
+		hash.Memory(32*1024),
+		hash.Threads(2),
+		hash.KeyLen(16),
+		hash.SaltLen(16),
+	)
+	defaultHasher := hash.NewArgon2Hasher()
+
+	require.False(t, reflect.DeepEqual(hasher, defaultHasher))
+}
 
 func TestArgon2Hasher(t *testing.T) {
 	t.Parallel()
